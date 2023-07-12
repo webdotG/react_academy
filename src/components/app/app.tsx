758931cdcdba1325c07b0389c16cb914ca1b6b1e
Grant Kirill <webdotg.inbox@gmail.com>
2023-07-12 21:04:01 +0300

    add route

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,27 @@
-import MainPage from '../../pages/main/main';
+import FavoritePage from '../../pages/favorite/favorite_page';
+import LoginPage from '../../pages/login/login_page';
+import MainPage from '../../pages/main/main_page';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import OfferPage from '../../pages/offer/offer_page';
+import NotFound from '../../pages/not_found/not_found_page';
 
 type appPageProps = {
   rentalOffer: number;
 }
 
-function App ({rentalOffer}:appPageProps) {
+function App({ rentalOffer }: appPageProps) {
 
   return (
-    <MainPage rentalOffer={rentalOffer} />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/main' element={<MainPage rentalOffer={rentalOffer}/> } />
+        <Route path='/favorite' element={<FavoritePage />} />
+        <Route path='/offer' element={<OfferPage />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App ;
+export default App;
