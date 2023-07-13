b4c5ff5be50fd5179cbc49249dc8f7438b135e2f
Grant Kirill <webdotg.inbox@gmail.com>
2023-07-13 10:40:12 +0300

    add private route

---
+++ src/components/private_route/private_route.tsx
@@ -0,0 +1,26 @@
+import { Navigate } from 'react-router-dom';
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
+
+type PrivateRouteProps = {
+  authorizationStatus: AuthorizationStatus;
+  children: JSX.Element;
+}
+
+
+function PrivateRoute(props: PrivateRouteProps) {
+
+  const { authorizationStatus, children } = props;
+
+  return (
+    authorizationStatus === AuthorizationStatus.Auth
+      ? children
+      : <Navigate to='/login' />
+  );
+}
+
+export default PrivateRoute;
