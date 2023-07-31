b7b9651c93e2b7302cfda265f7be6b3d63e8ad6b
Grant Kirill <webdotg.inbox@gmail.com>
2023-07-31 18:48:07 +0300

    add mouse event listCard

---
+++ src/components/card_list/card_list.tsx
@@ -0,0 +1,31 @@
+import { typeOffersList } from '../../types/type_offers';
+import Card from '../card/card';
+
+type CardListProps = {
+  offersList: typeOffersList[];
+  onListCardHover: (listItemId: string) => void;
+}
+
+function CardList({ offersList, onListCardHover }: CardListProps) {
+
+  return (
+    <div className="cities__places-list places__list tabs__content">
+      {offersList.map((item, ) => (
+        <Card
+          key={item.id}
+          id={item.id}
+          title={item.title}
+          type={item.type}
+          price={item.price}
+          image={item.image}
+          rating={item.rating}
+          isFavorite={item.isFavorite}
+          onListCardHover={onListCardHover}
+        />
+      )
+      )}
+    </div>
+  );
+}
+
+export default CardList;
